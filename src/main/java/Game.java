6dceaee2cfd1bb3093258360846707b364aa3a18
LordS3th <84471154+LordS3th@users.noreply.github.com>
2022-01-04 09:49:36 +0000

    Initial form for application

---
+++ src/main/java/Game.java
@@ -0,0 +1,61 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Game {
+    Screen screen;
+    private int x=10;
+    private int y=10;
+
+    public Game() {
+        try {
+            TerminalSize terminalSize = new TerminalSize(60, 60);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void draw() throws IOException{
+        screen.clear();
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+    public void run() {
+        try {
+            while(true) {
+                draw();
+                com.googlecode.lanterna.input.KeyStroke key = screen.readInput();
+                processKey(key);
+                if (key.getKeyType() == KeyType.Character && key.getCharacter() == ('q'))
+                    screen.close();
+                if (key.getKeyType() == KeyType.EOF)
+                    break;
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void processKey(com.googlecode.lanterna.input.KeyStroke key){
+        System.out.println(key);
+        switch (key.getKeyType()) {
+            case ArrowUp    -> y--;
+            case ArrowDown  -> y++;
+            case ArrowLeft  -> x--;
+            case ArrowRight -> x++;
+        }
+    }
+}
