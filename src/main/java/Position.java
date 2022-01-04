f60c77121fa2ae4dcda2e220895165ffcaece7b0
jsousa02 <jorgedaniel.sousa02@gmail.com>
2022-01-04 20:49:22 +0000

    create class Position

---
+++ src/main/java/Position.java
@@ -0,0 +1,23 @@
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * @return x coordinate
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @return y coordinate
+     */
+    public int getY() {
+        return y;
+    }
+}
