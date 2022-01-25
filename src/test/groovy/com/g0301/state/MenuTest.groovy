1041ffca226f6182a68a47d3c1411d64ce309804
jsousa02 <jorgedaniel.sousa02@gmail.com>
2022-01-25 21:06:44 +0000

    fix menu tests

--- src/test/groovy/com/g0301/state/MenuTest.groovy
+++ src/test/groovy/com/g0301/state/MenuTest.groovy
@@ -6,7 +6,13 @@ import spock.lang.Specification
 
 class MenuTest extends Specification {
 
-    MenuState menuState = new MenuState(new Game(60, 60, 30), new LanternaGUI(60, 60))
+    MenuState menuState
+    Game game = Mock()
+    LanternaGUI gui = Mock()
+
+    def setup() {
+        menuState = new MenuState(game, gui)
+    }
 
     def "Testing Menu Next Button"() {
         when: "we are in the third button and we press next button"
